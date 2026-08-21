7b70e1f87cde3dba8ba2cfaa1af6f605ae7508de
Valentin M <val.pro385@gmail.com>
2026-08-21 12:25:27 +0200

    Ajoute Edgard dans le groupe Charbo

--- backend/src/seed.js
+++ backend/src/seed.js
@@ -26,6 +26,7 @@ const users = [
   { username: 'Burak', role: 'worker' },
   { username: 'Bertrand', role: 'worker' },
   { username: 'Olivier', role: 'worker' },
+  { username: 'Edgard', role: 'worker' },
 
   // workers - equipe Tarare
   { username: 'Denis', role: 'worker' },
